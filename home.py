e02ef92e588165361bcf34c01406facc27c0a9bd
644230040petezavei <153893615+644230040petezavei@users.noreply.github.com>
2023-12-15 14:11:11 +0700

    Update home.py

--- home.py
+++ home.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 st.header("สุชาครีย์ อัครเศรณี")
 st.title('การทดสอบเขียนเว็บด้วย Python')
-st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
-st.subheader('สาขาวิชาวิทยาการข้อมูล')
+st.header('ผศ.ดร.สุชาครีย์')
+st.subheader('สาขาเทคโน')
 st.markdown("----")
 
 col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./pic/rung1.jfif')
+    st.image('./pic/rung1.jpg')
 with col2:
     st.image('./pic/iris2.jpg')
 
